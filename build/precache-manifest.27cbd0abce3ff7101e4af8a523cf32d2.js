39cffd0d652ff0d5fe84c6c3488afc70b6539c6f
vibin <vibin2joby@gmail.com>
2020-05-25 01:42:29 -0400

    initial commit

---
+++ build/precache-manifest.27cbd0abce3ff7101e4af8a523cf32d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b94e20b8fc609c8b8529e0640308ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ebfee37af66ca6e062",
+    "url": "/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "efb2740e85628be3fc4d",
+    "url": "/static/css/main.30de85b4.chunk.css"
+  },
+  {
+    "revision": "77ebfee37af66ca6e062",
+    "url": "/static/js/2.fbc83c47.chunk.js"
+  },
+  {
+    "revision": "18525d54fb5a585e368c5eb4da3d4104",
+    "url": "/static/js/2.fbc83c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb2740e85628be3fc4d",
+    "url": "/static/js/main.be3d265e.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  },
+  {
+    "revision": "03cfd7bcf39ceb6e0a84beda5d7b9a0b",
+    "url": "/static/media/Agustina-Signature.03cfd7bc.otf"
+  },
+  {
+    "revision": "b85b94151420522f069e65cd3fc99d59",
+    "url": "/static/media/about_me.b85b9415.jpg"
+  },
+  {
+    "revision": "abfd94f01d14af4ca838327ec2fab9f9",
+    "url": "/static/media/profile.abfd94f0.jpg"
+  },
+  {
+    "revision": "d3173c82707451e9425d82dd38313231",
+    "url": "/static/media/recommendations_1.d3173c82.png"
+  },
+  {
+    "revision": "908cbe17d0d57d6b5db5f175e4cb98f0",
+    "url": "/static/media/recommendations_2.908cbe17.png"
+  }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: ["./src/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./dist"
+  }
+};
